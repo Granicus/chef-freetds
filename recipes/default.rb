ebdbb17b3b00eb432033b7c63a6c1128766fecc6
Matt Kasa <mkasa@baent.net>
2013-07-10 17:34:14 -0700

    Remove configure and tsql check.

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,7 +11,6 @@ include_recipe 'build-essential'
 version = node['freetds']['version']
 freetds_url = node['freetds']['url'] ||
   "http://mirrors.ibiblio.org/freetds/stable/freetds-#{version}.tar.gz"
-configure_options = "--with-tdsver=#{node['freetds']['tds_version']} #{'--disable-odbc' unless node['freetds']['odbc']}"
 
 remote_file "#{Chef::Config[:file_cache_path]}/freetds-#{version}.tar.gz" do
   action :create_if_missing
@@ -24,10 +23,9 @@ bash "build freetds" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOF
     tar zxf freetds-#{version}.tar.gz
-    (cd freetds-#{version} && CFLAGS='-fPIC' ./configure #{configure_options})
+    (cd freetds-#{version} && CFLAGS='-fPIC')
     (cd freetds-#{version} && make && make install)
   EOF
-  not_if 'which tsql'
 end
 
 template "/usr/local/etc/freetds.conf" do
